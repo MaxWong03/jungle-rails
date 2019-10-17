bf091853ce5114f1e61e6cfb47531f634a0e6572
MaxWong03 <madmaxmmajournal@gmail.com>
2019-10-17 20:46:37 +0000

    finish user authentication feature

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
     post   :remove_item
   end
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+  
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
